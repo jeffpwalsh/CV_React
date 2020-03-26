073345fa26a02939c5eaf523a74b0eba92f699fc
jeffpwalsh <jeffpwalsh@gmail.com>
2020-03-26 11:42:35 +0200

    text edit to education

--- src/components/Education.js
+++ src/components/Education.js
@@ -9,8 +9,8 @@ const Education = () => {
              
             <h3> Education</h3>
             <hr />
-            <p> University of South Africa - ECONOMICS & MANAGEMENT SCIENCES - <em>2014</em> </p>
-            <p> University of South Africa - BACHELOR OF ACCOUNTING SCIENCES - <em>2019</em> </p>
+            <p> University of South Africa - ECONOMICS & MANAGEMENT SCIENCES - <em>2014-2015</em> </p>
+            <p> University of South Africa - BACHELOR OF ACCOUNTING SCIENCES - <em>2019-2022</em> </p>
             <p> Matric - <em>2002</em></p>
             <br />
             <p> HyperionDev - FULL STACK WEB DEVELOPMENT (MERN)  - <em>2020</em> </p>
