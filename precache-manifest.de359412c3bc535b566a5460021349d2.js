f34ff3778393f812e88d2a5e442ac13b73c934f2
jeffpwalsh <jeffpwalsh@gmail.com>
2020-03-26 11:32:00 +0200

    Updates

---
+++ precache-manifest.de359412c3bc535b566a5460021349d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "676a759151dcbcaad7b42344dfccc52c",
+    "url": "/CV_React/index.html"
+  },
+  {
+    "revision": "5113a0804c6444ec725a",
+    "url": "/CV_React/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "436826fff20bc94c8269",
+    "url": "/CV_React/static/css/main.2c9e6f8e.chunk.css"
+  },
+  {
+    "revision": "5113a0804c6444ec725a",
+    "url": "/CV_React/static/js/2.4c0e2db4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/CV_React/static/js/2.4c0e2db4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "436826fff20bc94c8269",
+    "url": "/CV_React/static/js/main.0d9cc7b7.chunk.js"
+  },
+  {
+    "revision": "abb895c66cdb1164d237",
+    "url": "/CV_React/static/js/runtime-main.66bdf774.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/CV_React/precache-manifest.de359412c3bc535b566a5460021349d2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CV_React/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
